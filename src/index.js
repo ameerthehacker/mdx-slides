1a4d907c867bfaf8e4e8dfb7406f7518a87a2117
Ameer Jhan <ameerjhanprof@gmail.com>
2021-05-02 15:35:20 +0530

    feat: hello world working

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
 const webpack = require('webpack');
 const path = require('path');
 const { logError, logInfo } = require('./utils');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const VirtualModulesPlugin = require('webpack-virtual-modules');
 
 function generateWebpackConfig(entryMDX) {
+  const entryFile = `
+    import React from 'react';
+    import ReactDOM  from 'react-dom';
+    import MDXDocument from './${entryMDX}';
+
+    ReactDOM.render(<MDXDocument />, document.getElementById('root'));
+  `;
   const cwd = process.cwd();
-  const entryPath = path.join(cwd, entryMDX);
+  const entryPath = path.join(cwd, './index.js');
   const outputPath = path.join(cwd, 'dist');
+  const htmlTemplatePath = path.join(__dirname, 'template.html');
 
   return {
     entry: entryPath,
     output: {
       path: outputPath,
+      clean: true,
     },
-    mode: 'production',
+    mode: 'development',
+    devtool: 'source-map',
     module: {
       rules: [
         {
@@ -21,19 +34,38 @@ function generateWebpackConfig(entryMDX) {
             {
               loader: 'babel-loader',
               options: {
-                presets: ['@babel/preset-react'],
+                presets: ['@babel/env', '@babel/preset-react'],
               },
             },
             {
               loader: '@mdx-js/loader',
+            },
+          ],
+        },
+        {
+          test: /\.js?$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
               options: {
-                remarkPlugins: [],
+                presets: ['@babel/env', '@babel/preset-react'],
               },
             },
           ],
         },
       ],
     },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Hello',
+        template: htmlTemplatePath,
+      }),
+      new NodePolyfillPlugin(),
+      new VirtualModulesPlugin({
+        './index.js': entryFile,
+      }),
+    ],
   };
 }
 
