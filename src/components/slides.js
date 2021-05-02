50d28bf3090058abd89f0579cbbf72e63648300e
Ameer Jhan <ameerjhanprof@gmail.com>
2021-05-02 22:38:07 +0530

    feat: navigating between slides

--- src/components/slides.js
+++ src/components/slides.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import { Redirect, useParams } from 'react-router-dom';
+import { Redirect, useHistory, useParams } from 'react-router-dom';
+import useKey from '@rooks/use-key';
 
 export default function Slides({ slides }) {
-  const { slideNumber } = useParams();
+  const history = useHistory();
+  let { slideNumber } = useParams();
+  slideNumber = parseInt(slideNumber);
   const slide = slides[slideNumber - 1];
+  const moveForward = () => {
+    const nextSlideNumber = slideNumber === slides.length ? 1 : slideNumber + 1;
+
+    history.push(`/${nextSlideNumber}`);
+  };
+  const moveBackward = () => {
+    const nextSlideNumber = slideNumber === 1 ? slides.length : slideNumber - 1;
+
+    history.push(`/${nextSlideNumber}`);
+  };
+
+  useKey(['ArrowLeft', 'ArrowDown'], () => moveBackward());
+  useKey(['ArrowRight', 'ArrowUp'], () => moveForward());
 
   // invalid slide number go to the first slide
   if (!slide) {
