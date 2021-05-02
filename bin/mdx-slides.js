79e44737c37abaefc2c43193b82227ba71ff5d10
Ameer Jhan <ameerjhanprof@gmail.com>
2021-05-02 22:42:36 +0530

    v0.0.1

--- bin/mdx-slides.js
+++ bin/mdx-slides.js
@@ -2,9 +2,16 @@
 const minimist = require('minimist');
 const { startDevServer, build } = require('../src');
 const { logError } = require('../src/utils');
+const pkg = require('../package.json');
 
 const argv = minimist(process.argv.slice(2));
 const args = argv._;
+
+if (argv.version) {
+  console.log(pkg.version);
+
+  return;
+}
 
 if (args.length === 1) {
   const [entryMDX] = args;
